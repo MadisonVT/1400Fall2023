220a8aba547ebdbff7e84d4cc4a318966e49d1a7
Madison VonTrapp~ <10995165@uvu.edu~>
2023-09-12 23:48:43 -0600

    Created code for Activity on Conditionals

---
+++ Unit01a/Something/Conditionals.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Program
+{
+    public class Programs
+    {
+        public static void Main()
+        {
+            int x = 20;
+            int y = 10;
+            string result = (x > y) ? "Hello World." : "Goodbye World.";
+            Console.WriteLine(result);
+        }
+    }
+}
