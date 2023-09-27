92ef8ca601d6b299aaff9b807364327d1cd3d5ee
Madison VT <10995165@uvu.edu>
2023-09-27 09:22:23 -0600

    Got rid of WIP, restaged old files

---
+++ TEST/DUCK/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the number of rows you want your pyramid to have!");
+        int num_rows = 5
+
+
+
+    }
+}
+
+
+
+
+
+
