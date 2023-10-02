2cd0a0fb14a6ca23aed146d0d02255d7824ef92a
Madison VonTrapp~ <10995165@uvu.edu~>
2023-10-02 10:02:22 -0600

    Started Favorite Foods Challenge

---
+++ Unit01c/Challenge1c2_FavoriteFoods.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Program
+{
+    public class Programs
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Tell me your 3 favorite foods!");
+            string[] foods = { };
+            int numFoods = foods.Count();
+
+            while (numFoods == 0)
+            {
+
+            }
+
+        }
+    }
+}
