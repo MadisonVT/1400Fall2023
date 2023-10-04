c6ed48dc1fb60d3318fc29b99de579c7092ff881
Madison VonTrapp~ <10995165@uvu.edu~>
2023-10-04 13:11:02 -0600

    Init Variables

--- Unit03_Unity/TestUnity/Assets/PlayerController.cs
+++ Unit03_Unity/TestUnity/Assets/PlayerController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float gravity = 9.81f;
+
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    private bool isJumping;
     // Start is called before the first frame update
     void Start()
     {
