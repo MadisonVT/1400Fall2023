6d1769a86d2aa04a310467654a05f8b7025a5170
Madison VT <10995165@uvu.edu>
2023-10-09 10:08:39 -0600

    Worked on PlayerController

--- Unit03_Unity/TestUnity/Assets/PlayerController.cs
+++ Unit03_Unity/TestUnity/Assets/PlayerController.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using System.Collections;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float gravity = 9.81f;
@@ -11,15 +10,21 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 moveDirection;
     private bool isJumping;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // Use this for initialization
+    void Start () {
+    controller = GetComponent<CharacterController>();
     }
-
+    
     // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        
+        //transform.position += Vector3.up * Time.deltaTime * moveSpeed;//
+        float horizonInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        var moveDirection = new Vector3(horizonInput, verticalInput) * moveSpeed;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            controller.Move(moveDirection * Time.deltaTime);
+        }
     }
 }
