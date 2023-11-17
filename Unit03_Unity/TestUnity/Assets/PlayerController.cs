5ebf7115726cec7a5c16ce43d7dd0bfac818649d
Madison VT <10995165@uvu.edu>
2023-11-17 10:23:18 -0700

    Basic Character Controller Added
    Not very good, object moves slowly

--- Unit03_Unity/TestUnity/Assets/PlayerController.cs
+++ Unit03_Unity/TestUnity/Assets/PlayerController.cs
@@ -3,26 +3,41 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float gravity = 9.81f;
 
-    private CharacterController characterController;
+    public CharacterController characterController;
     private Vector3 moveDirection;
     private bool isJumping;
+    
+    
     // Use this for initialization
     void Start () {
     characterController = GetComponent<CharacterController>();
     }
     
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
         //transform.position += Vector3.up * Time.deltaTime * moveSpeed;//
         float horizonInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         var moveDirection = new Vector3(horizonInput, verticalInput) * moveSpeed;
+
         if (Input.GetKeyDown(KeyCode.W))
+        {
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
         {
             characterController.Move(moveDirection * Time.deltaTime);
         }
