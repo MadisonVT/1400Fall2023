78adc05b5df4ad60beb649509c28a7939a468a08
Madison VT <10995165@uvu.edu>
2023-12-05 18:22:59 -0700

    Adding collision for UFOs
    Can now shoot and destroy enemies

--- Lab4a/UFO_Defense_Force/Assets/Scripts/SpawnEnemy.cs
+++ Lab4a/UFO_Defense_Force/Assets/Scripts/SpawnEnemy.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnEnemy : MonoBehaviour
 {
@@ -11,16 +13,24 @@ public class SpawnEnemy : MonoBehaviour
 
     private float spawnRangeX = 20f;
     private float spawnPosZ = 20f;
-    
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomUFO", startDelay, spawnInterval);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int ufoIndex = Random.Range(0, ufoPrefabs.Length);
-            Instantiate(ufoPrefabs[ufoIndex],spawnPos, ufoPrefabs[ufoIndex].transform.rotation);
-            
-        }
+        
+    }
+
+    void SpawnRandomUFO()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int ufoIndex = Random.Range(0, ufoPrefabs.Length);
+        Instantiate(ufoPrefabs[ufoIndex],spawnPos, ufoPrefabs[ufoIndex].transform.rotation);
     }
 }
