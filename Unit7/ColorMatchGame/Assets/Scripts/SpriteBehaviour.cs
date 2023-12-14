819f054dc7995b2a86cbbce81b1b92f9656d7a32
Madison VT <10995165@uvu.edu>
2023-12-14 14:57:29 -0700

    Added audio and game playable

--- Unit7/ColorMatchGame/Assets/Scripts/SpriteBehaviour.cs
+++ Unit7/ColorMatchGame/Assets/Scripts/SpriteBehaviour.cs
@@ -7,7 +7,7 @@ public class SpriteBehaviour : MonoBehaviour
 {
     private SpriteRenderer rendererObj;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rendererObj = GetComponent<SpriteRenderer>();
     }
